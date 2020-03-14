4c03126d8297772eaa6a34384dcb20a459b5e925
linziying <lingzi0827>
2020-03-14 12:42:13 +0800

    自适应布局

---
+++ my-project/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="app-container" id="app">
+    <div class="bg"></div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+    @import '../static/css/reset.css';
+    html{
+      height: 100%;
+    }
+    body,
+		.index-container,
+		.app-container,
+		.page-container {
+			width: 100%;
+			height: 100%;
+			overflow: hidden;
+		}
+		.app-container {
+			position: relative;
+		}
+		.page-container {
+			position: absolute;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			overflow: hidden;
+    }
+    .bg{
+			position: absolute;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			background: url('../static/img/bg.jpeg') no-repeat;
+			background-size: 100% 100%;
+		}
+</style>
---
+++ my-project/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="page-container">
+				<div class="content-container">
+					<div class="content">
+            DHDD
+		      </div>
+		    </div>
+		</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .content-container {
+			position: absolute;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			overflow: scroll;
+		}
+
+		.content{
+			width: 90%;
+			height: 80%;
+			background: #333;
+			position: absolute;
+			border-radius: 20px;
+			left: 50%;
+			top: 50%;
+			transform: translate(-50%, -50%);
+		}
+</style>
